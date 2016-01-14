5271128831e3d41542beabd41a040f23b61c7d0c
Jérémy Morin <jer.morin@free.fr>
2016-01-14 15:47:25 +0100

    feat(routing) small routing

---
+++ client/router.js
@@ -0,0 +1,16 @@
+Router.route('/', function () {
+    this.render('homepageTpl');
+});
+
+Router.route('/course/list/', function () {
+    this.render('courseListTpl');
+});
+
+Router.route('/course/create/', function () {
+    this.render('courseCreateTpl');
+});
+
+Router.route('/course/:_id', function () {
+    var course = Courses.findOne({_id: this.params._id});
+    this.render('courseTpl', {data: course});
+});
