15c33011c8c835be195bb96dc7102ec28fda0ac7
Jérémy Morin <jer.morin@free.fr>
2016-01-22 09:58:23 +0100

    feat(user) add first name and last name in account

---
+++ models/users.js
@@ -0,0 +1,24 @@
+Meteor.Schema = Meteor.Schema || {};
+
+Meteor.Schema.UserProfile = new SimpleSchema({
+    firstName: {
+        type: String,
+        label: 'First Name'
+    },
+    lastName: {
+        type: String,
+        label: 'Last Name',
+        optional: true
+    }
+});
+
+Meteor.user.schema = new SimpleSchema({
+    _id: {type: String},
+    emails: {type: Array},
+    'emails.$': {type: Object},
+    'emails.$.address': {type: String},
+    'emails.$.verified': {type: Boolean},
+    createdAt: {type: Date},
+    services: {type: Object, blackbox: true},
+    profile: {type: Meteor.Schema.UserProfile}
+});
