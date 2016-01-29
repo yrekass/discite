21cd750d4fa74c49e9a797eb20f47b70f3ce8d0d
Etienne Pichereau <Etienne Pichereau>
2016-01-29 09:40:52 +0100

    feat(Conference) Add screen sharing + chat

--- client/views/conference/detail/conference.js
+++ client/views/conference/detail/conference.js
@@ -1,16 +1,31 @@
+function appendStream(event, selector, template) {
+    if (!template.find('#'+selector+' video')) {
+        var div = template.find('#'+selector);
+        div.appendChild(event.mediaElement);
+    }
+}
+
 Template.conferenceTpl.rendered = function () {
     Meteor.subscribe('conference', Router.current().params._id);
     var params = {
-        MODERATOR_CHANNEL_ID: 'ABCDEF',
-        MODERATOR_SESSION_ID: 'XYZ',
+        MODERATOR_CAM_CHANNEL_ID: Router.current().params._id+'CAM',
+        MODERATOR_CAM_SESSION_ID: 'XYZ',
         MODERATOR_ID: 'JKL',
-        MODERATOR_SESSION: {
+        MODERATOR_CAM_SESSION: {
             audio: true,
             video: true,
             oneway:true
         },
-        MODERATOR_EXTRA: {}
+        MODERATOR_CAM_EXTRA: {},
+        MODERATOR_SCREEN_CHANNEL_ID: Router.current().params._id+'SCREEN',
+        MODERATOR_SCREEN_SESSION_ID: 'XYZ',
+        MODERATOR_SCREEN_SESSION: {
+            screen:true,
+            oneway:true
+        },
+        MODERATOR_SCREEN_EXTRA: {}
     };
+
     Session.set('paramsRtc', params);
 };
 
@@ -22,34 +37,59 @@ Template.conferenceTpl.helpers({
 
 Template.conferenceTpl.events({
     'click #launch': function (e, t) {
-        console.log(t);
         var params = Session.get('paramsRtc');
-        var moderator     = new RTCMultiConnection(params.MODERATOR_CHANNEL_ID);
-        moderator.session = params.MODERATOR_SESSION;
-        moderator.userid  = params.MODERATOR_ID;
-        moderator.extra   = params.MODERATOR_EXTRA;
-        moderator.open({
+        var moderator_cam     = new RTCMultiConnection(params.MODERATOR_CAM_CHANNEL_ID);
+        moderator_cam.session = params.MODERATOR_CAM_SESSION;
+        moderator_cam.userid  = params.MODERATOR_ID;
+        moderator_cam.extra   = params.MODERATOR_CAM_EXTRA;
+        moderator_cam.open({
             dontTransmit: true,
-            sessionid   : params.MODERATOR_SESSION_ID
+            sessionid   : params.MODERATOR_CAM_SESSION_ID
         });
-        moderator.onstream = function(event) {
-            var div = t.find('#stream');
-            div.appendChild(event.mediaElement);
+        moderator_cam.onstream = function (event)
+        {
+            appendStream(event, 'streamCam',t);
         };
+
+        var moderator_screen     = new RTCMultiConnection(params.MODERATOR_SCREEN_CHANNEL_ID);
+        moderator_screen.session = params.MODERATOR_SCREEN_SESSION;
+        moderator_screen.userid  = params.MODERATOR_ID;
+        moderator_screen.extra   = params.MODERATOR_SCREEN_EXTRA;
+        moderator_screen.open({
+            dontTransmit: true,
+            sessionid   : params.MODERATOR_SCREEN_SESSION_ID
+        });
+        moderator_screen.onstream = function (event)
+        {
+            appendStream(event, 'streamScreen',t);
+        }
     },
     'click #participate': function (e, t) {
         var params = Session.get('paramsRtc');
-        var participants = new RTCMultiConnection(params.MODERATOR_CHANNEL_ID);
-        participants.join({
-            sessionid: params.MODERATOR_SESSION_ID,
+        var participants_cam = new RTCMultiConnection(params.MODERATOR_CAM_CHANNEL_ID);
+        participants_cam.join({
+            sessionid: params.MODERATOR_CAM_SESSION_ID,
             userid   : params.MODERATOR_ID,
-            extra    : params.MODERATOR_EXTRA,
-            session  : params.MODERATOR_SESSION
+            extra    : params.MODERATOR_CAM_EXTRA,
+            session  : params.MODERATOR_CAM_SESSION
         });
 
-        participants.onstream = function(event) {
-            var div = t.find('#stream');
-            div.appendChild(event.mediaElement);
-        };
+        participants_cam.onstream =function (event)
+        {
+            appendStream(event, 'streamCam',t);
+        }
+
+        var participants_screen = new RTCMultiConnection(params.MODERATOR_SCREEN_CHANNEL_ID);
+        participants_screen.join({
+            sessionid: params.MODERATOR_SCREEN_SESSION_ID,
+            userid   : params.MODERATOR_ID,
+            extra    : params.MODERATOR_SCREEN_EXTRA,
+            session  : params.MODERATOR_SCREEN_SESSION
+        });
+
+        participants_screen.onstream = function (event)
+        {
+            appendStream(event, 'streamScreen',t);
+        }
     }
 });
